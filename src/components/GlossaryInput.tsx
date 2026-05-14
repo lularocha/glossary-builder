2da353c359ba8b3d21c31c437a78d2e8e1416161
Lula Rocha <lularocha@gmail.com>
2026-05-14 11:39:22 -0300

    Change intro text color from black to #777

--- src/components/GlossaryInput.tsx
+++ src/components/GlossaryInput.tsx
@@ -29,7 +29,7 @@ export const GlossaryInput: React.FC<GlossaryInputProps> = ({
 
   return (
     <div className="max-w-190 mx-auto">
-      <div className="text-base md:text-xl text-black mb-12 space-y-2">
+      <div className="text-base md:text-xl text-[#777] mb-12 space-y-2">
         <p>{ui.introParagraph1}</p>
         <p>{ui.introParagraph2}</p>
       </div>
